935cf5ebf17a87b6958abdc1df05ecc5617bfe48
ABDULLAHALSALEM <Abdullah.m122144@gmail.com>
2025-11-09 16:18:20 +0000

    Add main.h header and prototypes for simple shell 0.1

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int is_blank(const char *s);
+char *trim_spaces(char *s);
+void execute_command(char *line);
+
+extern char **environ;
+
+#endif /* MAIN_H */
